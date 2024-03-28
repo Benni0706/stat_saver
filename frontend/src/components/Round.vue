05e11c2d4754f7a4aae43e80691da2f43a504406
Benni <61964978+Benni0706@users.noreply.github.com>
2024-03-28 11:58:52 +0100

    fixed missing round color (#2)

--- frontend/src/components/Round.vue
+++ frontend/src/components/Round.vue
@@ -2,21 +2,21 @@
 const props = defineProps(['round'])
 
 function teamColor(team) {
-    let color = "gray-500"
+    let color = "bg-gray-500"
     if (team == "innocents") {
-        color = "green-600"
+        color = "bg-green-600"
     } else if (team == "traitors") {
-        color = "red-600"
+        color = "bg-red-600"
     } else if (team == "nones") {
-        color = "gray-500"
+        color = "bg-gray-500"
     } else if (team == "clowns" || team == "jesters") {
-        color = "purple-500"
+        color = "bg-purple-500"
     } else if (team == "serialkillers") {
-        color = "cyan-700"
+        color = "bg-cyan-700"
     } else if (team == "dunces") {
-        color = "pink-500"
+        color = "bg-pink-500"
     } else {
-        color = "orange-950"
+        color = "bg-orange-950"
     }
     return color
 }
@@ -24,7 +24,7 @@ function teamColor(team) {
 
 <template>
     <div class="rounded-lg mb-1 hover:cursor-pointer">
-        <button :class="'grid grid-cols-4 p-1 w-full rounded-lg hover:opacity-80 peer focus:opacity-80 bg-' + teamColor(props.round.winner_team)">
+        <button class="grid grid-cols-4 p-1 w-full rounded-lg hover:opacity-80 peer focus:opacity-80" :class="teamColor(props.round.winner_team)">
             <span class="text-left">{{ props.round.date }}</span>
             <span class="text-left">{{ props.round.winner_team }}</span>
             <span class="text-left">{{ props.round.player_count }}</span>
